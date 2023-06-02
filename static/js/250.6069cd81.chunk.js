a864a6a50fad97b237be69a8d8877d2c30cf582e
Drimak-Stepan <108022018+Drimak-Stepan@users.noreply.github.com>
2023-06-02 21:19:12 +0000

    Deploying to gh-pages from @ Drimak-Stepan/cards-tweets@f72ec7da1eb95a8ed771f05b16f38f5c32b77d3c 🚀

---
+++ static/js/250.6069cd81.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcards_tweets=self.webpackChunkcards_tweets||[]).push([[250],{192:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var i,r=t(168),c=t(444).ZP.h1(i||(i=(0,r.Z)(["\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  margin-top: 20px;\n  width: 280px;\n  font-weight: 600;\n  font-size: 32px;\n  line-height: 1.38;\n  color: black;\n"]))),s=t(184),u=function(){return(0,s.jsx)(c,{children:"Home"})},f=function(){return(0,s.jsx)(u,{})}}}]);
+//# sourceMappingURL=250.6069cd81.chunk.js.map
