d48f5307c7dcec1e62cf4a9d0dffbf69e996acd1
Drimak-Stepan <drimak103@gmail.com>
2023-06-04 08:46:56 +0300

    fixBug

--- src/shared/components/Button/Button.styles.js
+++ src/shared/components/Button/Button.styles.js
@@ -17,7 +17,7 @@ export const Btn = styled.button`
 
   width: 196px;
   height: 50px;
-  padding: 14px;
+  padding: 14px 28px;
 
   font-family: Montserrat;
   font-weight: 600;
@@ -25,7 +25,7 @@ export const Btn = styled.button`
   line-height: 1.2;
   background-color: #ebd8ff;
   color: #373737;
-  border-radius: 3.44px;
+  border-radius: 10px;
   border: none;
   outline: none;
   box-shadow: 0px 3.44px 3.44px 0px rgba(0, 0, 0, 0.25);
